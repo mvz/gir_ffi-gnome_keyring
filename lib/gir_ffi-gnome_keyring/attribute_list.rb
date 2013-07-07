d9db50a9634f848a55ab738a8dfbae79e0085c96
Matijs van Zuijlen <matijs@matijs.net>
2013-07-07 21:56:05 +0200

    Start implementing AttributeList.new

--- lib/gir_ffi-gnome_keyring/attribute_list.rb
+++ lib/gir_ffi-gnome_keyring/attribute_list.rb
@@ -1,5 +1,14 @@
 module GnomeKeyring
-  class AttributeList < GLib::Array
+  load_class :Attribute
 
+  class Attribute
+    setup_method 'list_new'
+  end
+
+  class AttributeList < GLib::Array
+    def self.new
+      ptr = GnomeKeyring::Lib.gnome_keyring_attribute_list_new
+      wrap(GnomeKeyring::Attribute, ptr)
+    end
   end
 end
--- test/unit/gir_ffi-gnome_keyring/attribute_list_test.rb
+++ test/unit/gir_ffi-gnome_keyring/attribute_list_test.rb
@@ -4,4 +4,10 @@ describe GnomeKeyring::AttributeList do
   it "descends from GLib::Array" do
     GnomeKeyring::AttributeList.ancestors.must_include GLib::Array
   end
+  describe "#new" do
+    it "takes no arguments" do
+      GnomeKeyring::AttributeList.new
+      pass
+    end
+  end
 end
