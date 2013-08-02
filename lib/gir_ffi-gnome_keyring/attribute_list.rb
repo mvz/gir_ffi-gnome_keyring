af6259d45e7b0bf3989c509f655a37f7c314de1e
Matijs van Zuijlen <matijs@matijs.net>
2013-08-02 16:13:16 +0200

    Implement AttributeList#append_string

--- lib/gir_ffi-gnome_keyring/attribute_list.rb
+++ lib/gir_ffi-gnome_keyring/attribute_list.rb
@@ -3,12 +3,25 @@ module GnomeKeyring
 
   class Attribute
     setup_method 'list_new'
+    setup_method 'list_append_string'
   end
 
   class AttributeList < GLib::Array
     def self.new
       ptr = GnomeKeyring::Lib.gnome_keyring_attribute_list_new
       wrap(GnomeKeyring::Attribute, ptr)
+    end
+
+    # FIXME: Remove once GLib::Array#index has been fixed
+    def index idx
+      ptr = @struct[:data] + idx * get_element_size
+      GnomeKeyring::Attribute.wrap(ptr)
+    end
+
+    def append_string name, value
+      _v2 = GirFFI::InPointer.from(:utf8, name)
+      _v3 = GirFFI::InPointer.from(:utf8, value)
+      GnomeKeyring::Lib.gnome_keyring_attribute_list_append_string self, _v2, _v3
     end
 
     private
--- test/unit/gir_ffi-gnome_keyring/attribute_list_test.rb
+++ test/unit/gir_ffi-gnome_keyring/attribute_list_test.rb
@@ -4,10 +4,22 @@ describe GnomeKeyring::AttributeList do
   it "descends from GLib::Array" do
     GnomeKeyring::AttributeList.ancestors.must_include GLib::Array
   end
-  describe "#new" do
+
+  describe ".new" do
     it "takes no arguments" do
       GnomeKeyring::AttributeList.new
       pass
+    end
+  end
+
+  describe "#append_string" do
+    it "appends a string attribute" do
+      list = GnomeKeyring::AttributeList.new
+      list.append_string 'foo', 'bar'
+      attr = list.index(0)
+      attr.name.must_equal 'foo'
+      attr.get_string.must_equal 'bar'
+      attr.type.must_equal :string
     end
   end
 end
