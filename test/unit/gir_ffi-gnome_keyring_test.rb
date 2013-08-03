58d46c9af93c5a18b3c739064787ffa6f9b19c2d
Matijs van Zuijlen <matijs@matijs.net>
2013-08-03 08:00:53 +0200

    Use stubs to test GnomeKeyring.find_items_sync

--- test/unit/gir_ffi-gnome_keyring_test.rb
+++ test/unit/gir_ffi-gnome_keyring_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 describe GnomeKeyring do
   describe ".find_items_sync" do
-    it "returns a tuple of result code and result list" do
-      code, list = GnomeKeyring.find_items_sync :generic_secret, []
-      code.must_equal :ok
-      list.element_type.must_equal GnomeKeyring::Found
+    it "calls Lib.gnome_keyring_find_items_sync with the correct values" do
+      body = lambda { |type, attrs, listptr|
+        type.must_equal :generic_secret
+        attrs.must_be_instance_of GnomeKeyring::AttributeList
+        listptr.must_be_instance_of GirFFI::InOutPointer
+        :ok
+      }
+
+      GnomeKeyring::Lib.stub :gnome_keyring_find_items_sync, body do
+        code, list = GnomeKeyring.find_items_sync :generic_secret, []
+        code.must_equal :ok
+        list.must_be_nil
+      end
     end
   end
 end
