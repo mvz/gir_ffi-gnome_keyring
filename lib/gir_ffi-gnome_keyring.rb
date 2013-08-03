ebfbf7671e7d108ff5db032d95b915c0f6238656
Matijs van Zuijlen <matijs@matijs.net>
2013-08-03 07:30:03 +0200

    Implement GnomeKeyring#find_items_sync

--- lib/gir_ffi-gnome_keyring.rb
+++ lib/gir_ffi-gnome_keyring.rb
@@ -5,3 +5,19 @@ GirFFI.setup 'GnomeKeyring'
 require 'gir_ffi-gnome_keyring/attribute'
 require 'gir_ffi-gnome_keyring/attribute_list'
 require 'gir_ffi-gnome_keyring/found'
+
+module GnomeKeyring
+  setup_method :find_items_sync
+
+  class << self
+    remove_method :find_items_sync
+  end
+
+  def self.find_items_sync(type, attributes)
+    _v2 = GnomeKeyring::AttributeList.from(attributes)
+    _v3 = GirFFI::InOutPointer.for(:glist)
+    _v4 = GnomeKeyring::Lib.gnome_keyring_find_items_sync(type, _v2, _v3)
+    _v5 = GLib::List.wrap(GnomeKeyring::Found, _v3.to_value)
+    return [_v4, _v5]
+  end
+end
--- test/unit/gir_ffi-gnome_keyring_test.rb
+++ test/unit/gir_ffi-gnome_keyring_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 describe GnomeKeyring do
-  it "exists" do
-    pass
+  describe ".find_items_sync" do
+    it "returns a tuple of result code and result list" do
+      code, list = GnomeKeyring.find_items_sync :generic_secret, []
+      code.must_equal :ok
+      list.element_type.must_equal GnomeKeyring::Found
+    end
   end
 end
