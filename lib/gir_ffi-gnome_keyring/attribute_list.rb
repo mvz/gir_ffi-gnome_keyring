51958302471005f2c03b68771ec4b08210b3a682
Matijs van Zuijlen <matijs@matijs.net>
2017-09-22 10:27:03 +0200

    Autocorrect Style/MethodDefParentheses

--- lib/gir_ffi-gnome_keyring/attribute_list.rb
+++ lib/gir_ffi-gnome_keyring/attribute_list.rb
@@ -7,11 +7,11 @@ module GnomeKeyring
       wrap(ptr)
     end
 
-    def self.wrap ptr
+    def self.wrap(ptr)
       super GnomeKeyring::Attribute, ptr
     end
 
-    def self.from it
+    def self.from(it)
       case it
       when self then it
       when FFI::Pointer then wrap it
@@ -19,13 +19,13 @@ module GnomeKeyring
       end
     end
 
-    def append_string name, value
+    def append_string(name, value)
       _v2 = GirFFI::InPointer.from(:utf8, name)
       _v3 = GirFFI::InPointer.from(:utf8, value)
       GnomeKeyring::Lib.gnome_keyring_attribute_list_append_string self, _v2, _v3
     end
 
-    def append_uint32 name, value
+    def append_uint32(name, value)
       _v2 = GirFFI::InPointer.from(:utf8, name)
       GnomeKeyring::Lib.gnome_keyring_attribute_list_append_uint32 self, _v2, value
     end
