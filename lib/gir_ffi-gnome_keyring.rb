557db3923fef0ba17e0a5c82d74028766155a917
Matijs van Zuijlen <matijs@matijs.net>
2017-09-22 11:09:08 +0200

    Compact extra lines

--- lib/gir_ffi-gnome_keyring.rb
+++ lib/gir_ffi-gnome_keyring.rb
@@ -35,10 +35,8 @@ module GnomeKeyring
     v4 = GnomeKeyring::AttributeList.from(attributes)
     v5 = GirFFI::InPointer.from_utf8(secret)
     v7 = FFI::MemoryPointer.new :uint32
-    v8 = GnomeKeyring::Lib.gnome_keyring_item_create_sync(v1, type, v3, v4,
-                                                          v5,
-                                                          update_if_exists,
-                                                          v7)
+    v8 = GnomeKeyring::Lib.gnome_keyring_item_create_sync(v1, type, v3, v4, v5,
+                                                          update_if_exists, v7)
     v9 = v7.get_uint32(0)
     [v8, v9]
   end
