980cfa6e6ad3b2fcaba619fe8ac17893525da3c3
Matijs van Zuijlen <matijs@matijs.net>
2020-07-29 20:31:38 +0200

    Fix minitest syntax

--- test/unit/gir_ffi-gnome_keyring/attribute_test.rb
+++ test/unit/gir_ffi-gnome_keyring/attribute_test.rb
@@ -5,37 +5,37 @@ require "test_helper"
 describe GnomeKeyring::Attribute do
   describe ".list_new" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_new } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_new }).must_raise NoMethodError
     end
   end
 
   describe ".list_append_string" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_append_string } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_append_string }).must_raise NoMethodError
     end
   end
 
   describe ".list_append_uint32" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_append_uint32 } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_append_uint32 }).must_raise NoMethodError
     end
   end
 
   describe ".list_copy" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_copy } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_copy }).must_raise NoMethodError
     end
   end
 
   describe ".list_free" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_free } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_free }).must_raise NoMethodError
     end
   end
 
   describe ".list_to_glist" do
     it "is private" do
-      _ { proc { GnomeKeyring::Attribute.list_to_glist } }.must_raise NoMethodError
+      _(proc { GnomeKeyring::Attribute.list_to_glist }).must_raise NoMethodError
     end
   end
 end
