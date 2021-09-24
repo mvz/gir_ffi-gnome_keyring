c7f2392cf5228006089c286ecc36769c76433268
Matijs van Zuijlen <matijs@matijs.net>
2021-09-24 11:59:54 +0200

    Update coding style to match standardb
    The code now registers no offenses with either standardb or RuboCop. In
    future, more adjustments to the RuboCop configuration may be needed
    
    - Updates RuboCop configuration for more standardrb compatibility
    - Corrects new offenses

--- lib/gir_ffi-gnome_keyring.rb
+++ lib/gir_ffi-gnome_keyring.rb
@@ -30,15 +30,16 @@ module GnomeKeyring
     [v4, v5]
   end
 
-  def self.item_create_sync(keyring, type, display_name, attributes, secret,
-                            update_if_exists)
+  def self.item_create_sync(
+    keyring, type, display_name, attributes, secret, update_if_exists
+  )
     v1 = GirFFI::InPointer.from_utf8(keyring)
     v3 = GirFFI::InPointer.from_utf8(display_name)
     v4 = GnomeKeyring::AttributeList.from(attributes)
     v5 = GirFFI::InPointer.from_utf8(secret)
     v7 = FFI::MemoryPointer.new :uint32
-    v8 = GnomeKeyring::Lib.gnome_keyring_item_create_sync(v1, type, v3, v4, v5,
-                                                          update_if_exists, v7)
+    v8 = GnomeKeyring::Lib
+      .gnome_keyring_item_create_sync(v1, type, v3, v4, v5, update_if_exists, v7)
     v9 = v7.get_uint32(0)
     [v8, v9]
   end
